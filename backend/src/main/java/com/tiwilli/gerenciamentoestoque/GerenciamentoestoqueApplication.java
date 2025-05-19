cd9012a2fcd9e0c45398cf491aa88cf0fa1d373e
Willian Comiotto <willianbcomiotto@gmail.com>
2025-05-19 10:41:13 -0300

    first commit

---
+++ backend/src/main/java/com/tiwilli/gerenciamentoestoque/GerenciamentoestoqueApplication.java
@@ -0,0 +1,13 @@
+package com.tiwilli.gerenciamentoestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoestoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoestoqueApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/tiwilli/gerenciamentoestoque/GerenciamentoestoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiwilli.gerenciamentoestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoestoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
